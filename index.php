e57a91831a51a4824ee5a05cf8d463f34e16a8aa
Alexey Gvozdev <gvozdev.ua@gmail.com>
2016-10-04 16:24:28 +0300

    Re-initialize repository without old history.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+Class One {
+
+function foo() {
+
+echo "Hello from class One!";
+
+}
+
+function callMe() {
+
+$this->foo();
+
+}
+
+}
+
+Class Two extends One {
+
+function foo() {
+
+echo "Hello from class Two";
+
+}
+
+}
+
+$Two = new two();
+
+$Two->callMe();
+echo 'dfdf';
+echo 'dfdf';
+
+?>
---
+++ menu.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+<head>
+<title>Magazine</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<!--<link rel="stylesheet" type="text/css" href="css/style.css" />-->
+<style>
+	body{ background-color: #ccc; }
+	.menu { list-style: none; }
+	.menu li{ float: left; 
+			margin: 0 20px 0 0;
+			background: #044fac;
+			border-radius: 5px;
+			width: 150px;
+			text-align: center;
+			line-height: 50px; 
+			}
+	.menu a{ color: #fff;
+			display: block;
+			height: 50px;
+			font-size: 20px;
+			text-decoration: none;
+	}
+	.menu a:hover{
+			background: #b4145b;
+			border-radius: 5px;
+			transition-property: background;
+			transition-duration: 1s 
+			
+	}
+</style>
+<!--  -->
+</head>
+<body>
+
+	<ul class="menu">
+		<li><a href="#">Главная</a></li>
+		<li><a href="#">О нас</a></li>
+		<li><a href="#">Галерея</a></li>
+		<li><a href="#">Прайс</a></li>
+		<li><a href="#">Контакты</a></li>
+	</ul>
+	
+	
+	
+	
+</div>
+</body>
+</html>
